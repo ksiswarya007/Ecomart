5f5d41e414dbdcb260810c53c59d0a4e30fb4688
ksiswarya007 <136336216+ksiswarya007@users.noreply.github.com>
2025-07-11 23:34:41 +0530

    added verccel frontend url to server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const port=process.env.PORT||4000;
 await connectDB()
 await connectCloudinary()
 //allow multiple origins
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173','https://ecomart-final.vercel.app']
 
 app.post('/stripe',express.raw({type:'application/json'}),stripeWebhooks)
 
